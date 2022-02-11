81ff5b97faab6ba70e0ea5559d949c367db0ee14
Randall Wilk <wilk.randall@gmail.com>
2022-02-11 11:18:29 -0600

    Add convenience methods to custom exceptions

--- src/Exceptions/PrintTaskFailed.php
+++ src/Exceptions/PrintTaskFailed.php
@@ -10,7 +10,27 @@ class PrintTaskFailed extends Exception
 {
     public static function missingPrinterId(): self
     {
-        return new static('A printer must be specified to print');
+        return new static('A printer must be specified to print!');
+    }
+
+    public static function missingSource(): self
+    {
+        return new static('A print source must be specified!');
+    }
+
+    public static function missingContentType(): self
+    {
+        return new static('Content type must be specified for this driver!');
+    }
+
+    public static function noContent(): self
+    {
+        return new static('No content was provided for the print job!');
+    }
+
+    public static function noJobCreated(): self
+    {
+        return new static('The print job failed to create.');
     }
 
     public static function driverFailed(string $message): self
