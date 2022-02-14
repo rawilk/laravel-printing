1ae574384935a3e6b3bfd7874f7cfa1d1009975f
Randall Wilk <wilk.randall@gmail.com>
2022-02-14 11:50:56 -0600

    Remove ArrayAccess implementation

--- src/Api/PrintNode/Entity/Entity.php
+++ src/Api/PrintNode/Entity/Entity.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Rawilk\Printing\Api\PrintNode\Entity;
 
-use ArrayAccess;
 use Carbon\Carbon;
 use Illuminate\Contracts\Support\Arrayable;
 use JsonSerializable;
 use ReflectionObject;
 use ReflectionProperty;
 
-abstract class Entity implements Arrayable, JsonSerializable, ArrayAccess
+abstract class Entity implements Arrayable, JsonSerializable
 {
     public function __construct(array $data = [])
     {
@@ -59,36 +58,5 @@ abstract class Entity implements Arrayable, JsonSerializable, ArrayAccess
     public function jsonSerialize(): mixed
     {
         return $this->toArray();
-    }
-
-    public function offsetExists(mixed $offset): bool
-    {
-        return property_exists($this, $offset) && isset($this->{$offset});
-    }
-
-    public function offsetGet(mixed $offset): mixed
-    {
-        if (! property_exists($this, $offset)) {
-            return null;
-        }
-
-        return $this->{$offset};
-    }
-
-    public function offsetSet(mixed $offset, mixed $value): void
-    {
-        if (property_exists($this, $offset)) {
-            $this->{$offset} = $value;
-        }
-    }
-
-    public function offsetUnset(mixed $offset): void
-    {
-        if (! property_exists($this, $offset)) {
-            return;
-        }
-
-        $freshInstance = new static;
-        $this->{$offset} = $freshInstance->{$offset};
     }
 }
