68205987bd25747ca20127738db500060404288f
Randall Wilk <wilk.randall@gmail.com>
2022-02-14 11:23:14 -0600

    Update php cs fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,28 +10,40 @@ $finder = Symfony\Component\Finder\Finder::create()
     ])
     ->name('*.php')
     ->notName('*.blade.php')
+    ->notName('Timezone.php') // disable it on this class for now to prevent linting errors
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config)
     ->setRules([
-        '@PSR2' => true,
+        '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sortAlgorithm' => 'alpha'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => true,
         'phpdoc_scalar' => true,
         'unary_operator_spaces' => true,
         'binary_operator_spaces' => true,
+        'new_with_braces' => false,
         'blank_line_before_statement' => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
         'phpdoc_single_line_var_spacing' => true,
         'phpdoc_var_without_name' => true,
+        'class_attributes_separation' => [
+            'elements' => [
+                'method' => 'one',
+            ],
+        ],
         'method_argument_space' => [
             'on_multiline' => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
-        ]
+        ],
+        // TODO: enable once the github action uses the 3.1.0 release of php-cs-fixer
+        // 'types_spaces' => [
+        //     'space' => 'none',
+        // ],
+        'single_trait_insert_per_statement' => false,
     ])
     ->setFinder($finder);
