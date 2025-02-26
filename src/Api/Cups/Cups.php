566035e7ab29d18e0be78566e5b8e4f2353fe9e1
Randall Wilk <22842525+rawilk@users.noreply.github.com>
2025-02-26 10:57:15 -0600

    wip

--- src/Api/Cups/Cups.php
+++ src/Api/Cups/Cups.php
@@ -5,43 +5,46 @@ declare(strict_types=1);
 namespace Rawilk\Printing\Api\Cups;
 
 use Illuminate\Support\Facades\Http;
+use Rawilk\Printing\Api\Cups\Exceptions\ServerError;
 
 class Cups
 {
     public function __construct(
-        private string $ip,
-        private ?string $username,
-        private ?string $password,
-        private int $port = 631,
-        private bool $secure = false
+        protected string $ip,
+        protected ?string $username,
+        protected ?string $password,
+        protected int $port = 631,
+        protected bool $secure = false
     ) {}
 
-    /**
-     * @throws Illuminate\Http\Client\ConnectionException
-     * @throws Illuminate\Http\Client\RequestException
-     * @throws \Rawilk\Printing\Api\Cups\Exceptions\ClientException
-     * @throws \Rawilk\Printing\Api\Cups\Exceptions\ServerException
-     */
     public function makeRequest(Request $request): Response
     {
-        $http = Http::withBody($request->encode());
-
-        if ($this->username || $this->password) {
-            $http->withBasicAuth($this->username, $this->password);
-        }
-
-        $http = $http->withHeaders(
-            [
+        $http = Http::withBody($request->encode())
+            ->when(
+                $this->username || $this->password,
+                fn (Http $http) => $http->withBasicAuth($this->username ?? '', $this->password ?? ''),
+            )
+            ->withHeaders([
                 'Content-Type' => 'application/ipp',
-            ]
-        )->post($this->getScheme() . '://' . $this->ip . ':' . $this->port . '/admin')
-            ->throwIfClientError();
-        $response = new Response($http->body());
+            ]);
 
-        return $response;
+        $response = $http->post($this->getAdminUrl())
+            ->throwIfClientError();
+
+        throw_unless(
+            $response->ok(),
+            new ServerError('Cups server request failed.'),
+        );
+
+        return new Response($response->body());
     }
 
-    private function getScheme()
+    protected function getAdminUrl(): string
+    {
+        return $this->getScheme() . '://' . $this->ip . ':' . $this->port . '/admin';
+    }
+
+    protected function getScheme(): string
     {
         return $this->secure ? 'https' : 'http';
     }
