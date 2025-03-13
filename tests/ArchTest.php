0aa242c6f82fe9dafb1d9bf26c17085fe923765b
Randall Wilk <22842525+rawilk@users.noreply.github.com>
2025-03-13 10:54:45 -0500

    Skip arch tests if arch() function is not defined

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -18,111 +18,113 @@ use Rawilk\Printing\Exceptions\ExceptionInterface;
 use Rawilk\Printing\Exceptions\PrintingException;
 use Rawilk\Printing\PrintingServiceProvider;
 
-arch()->preset()->security();
+describe('architecture testing', function () {
+    arch()->preset()->security();
 
-arch('strict types')->expect('Rawilk\Printing')->toUseStrictTypes();
-arch('strict equality')->expect('Rawilk\Printing')->toUseStrictEquality();
+    arch('strict types')->expect('Rawilk\Printing')->toUseStrictTypes();
+    arch('strict equality')->expect('Rawilk\Printing')->toUseStrictEquality();
 
-arch('globals')->expect([
-    'dd',
-    'ddd',
-    'dump',
-    'env',
-    'exit',
-    'ray',
+    arch('globals')->expect([
+        'dd',
+        'ddd',
+        'dump',
+        'env',
+        'exit',
+        'ray',
 
-    // strict preset
-    'sleep',
-    'usleep',
-])->not->toBeUsed();
+        // strict preset
+        'sleep',
+        'usleep',
+    ])->not->toBeUsed();
 
-arch('no final classes')
-    ->expect('Rawilk\Printing')
-    ->classes()
-    ->not->toBeFinal()->ignoring([
-        PrintingServiceProvider::class,
-        PrintNode::class,
-        Cups::class,
-    ]);
-
-arch('contracts')->expect('Rawilk\Printing\Contracts')
-    ->not->toHaveSuffix('Interface')
-    ->not->toHaveSuffix('Contract')
-    ->toBeInterfaces();
-
-arch('enums')->expect('Rawilk\Printing\Enums')
-    ->toBeEnums()
-    ->not->toHaveSuffix('Enum');
-
-arch('exceptions')->expect('Rawilk\Printing\Exceptions')
-    ->classes()
-    ->not->toHaveSuffix('Exception')->ignoring([
-        PrintingException::class,
-    ])
-    ->toExtend(Throwable::class)
-    ->toImplement(ExceptionInterface::class);
-
-arch('facades')->expect('Rawilk\Printing\Facades')
-    ->toExtend(Facade::class)
-    ->not->toHaveSuffix('Facade');
-
-arch('concerns')->expect('Rawilk\Printing\Concerns')
-    ->toBeTraits();
-
-describe('cups api', function (): void {
-    arch('attributes')->expect('Rawilk\Printing\Api\Cups\Attributes')
+    arch('no final classes')
+        ->expect('Rawilk\Printing')
         ->classes()
-        ->toExtend(AttributeGroup::class);
-
-    arch('enums')->expect('Rawilk\Printing\Api\Cups\Enums')
-        ->toBeEnums()
-        ->not->toHaveSuffix('Enum');
-
-    arch('exceptions')->expect('Rawilk\Printing\Api\Cups\Exceptions')
-        ->not->toHaveSuffix('Exception')
-        ->toExtend(PrintingException::class)
-        ->toOnlyBeUsedIn('Rawilk\Printing\Api\Cups');
-
-    arch('types')->expect('Rawilk\Printing\Api\Cups\Types')
-        ->classes()
-        ->toExtend(Type::class);
-
-    arch('resources')->expect('Rawilk\Printing\Api\Cups\Resources')
-        ->classes()
-        ->toExtend(CupsObject::class);
-
-    arch('services')->expect('Rawilk\Printing\Api\Cups\Service')
-        ->classes()
-        ->toExtend(CupsAbstractService::class)->ignoring([CupsServiceFactory::class])
-        ->toHaveSuffix('Service')->ignoring([CupsServiceFactory::class]);
-});
-
-describe('printnode api', function () {
-    arch('enums')->expect('Rawilk\Printing\Api\PrintNode\Enums')
-        ->toBeEnums()
-        ->not->toHaveSuffix('Enum');
-
-    arch('exceptions')->expect('Rawilk\Printing\Api\PrintNode\Exceptions')
-        ->toImplement(ExceptionInterface::class)
-        ->not->toHaveSuffix('Exception');
-
-    arch('resources')->expect('Rawilk\Printing\Api\PrintNode\Resources')
-        ->classes()
-        ->toExtend(PrintNodeObject::class);
-
-    arch('resource concerns')->expect('Rawilk\Printing\Api\PrintNode\Resources\Concerns')
-        ->toBeTraits()
-        ->toOnlyBeUsedIn('Rawilk\Printing\Api\PrintNode\Resources');
-
-    arch('resource operations')->expect('Rawilk\Printing\Api\PrintNode\Resources\ApiOperations')
-        ->toBeTraits()
-        ->toOnlyBeUsedIn([
-            'Rawilk\Printing\Api\PrintNode\Resources',
-            PrintNodeApiResource::class,
+        ->not->toBeFinal()->ignoring([
+            PrintingServiceProvider::class,
+            PrintNode::class,
+            Cups::class,
         ]);
 
-    arch('services')->expect('Rawilk\Printing\Api\PrintNode\Service')
+    arch('contracts')->expect('Rawilk\Printing\Contracts')
+        ->not->toHaveSuffix('Interface')
+        ->not->toHaveSuffix('Contract')
+        ->toBeInterfaces();
+
+    arch('enums')->expect('Rawilk\Printing\Enums')
+        ->toBeEnums()
+        ->not->toHaveSuffix('Enum');
+
+    arch('exceptions')->expect('Rawilk\Printing\Exceptions')
         ->classes()
-        ->toExtend(PrintNodeAbstractService::class)->ignoring([PrintNodeServiceFactory::class])
-        ->toHaveSuffix('Service')->ignoring([PrintNodeServiceFactory::class]);
-});
+        ->not->toHaveSuffix('Exception')->ignoring([
+            PrintingException::class,
+        ])
+        ->toExtend(Throwable::class)
+        ->toImplement(ExceptionInterface::class);
+
+    arch('facades')->expect('Rawilk\Printing\Facades')
+        ->toExtend(Facade::class)
+        ->not->toHaveSuffix('Facade');
+
+    arch('concerns')->expect('Rawilk\Printing\Concerns')
+        ->toBeTraits();
+
+    describe('cups api', function (): void {
+        arch('attributes')->expect('Rawilk\Printing\Api\Cups\Attributes')
+            ->classes()
+            ->toExtend(AttributeGroup::class);
+
+        arch('enums')->expect('Rawilk\Printing\Api\Cups\Enums')
+            ->toBeEnums()
+            ->not->toHaveSuffix('Enum');
+
+        arch('exceptions')->expect('Rawilk\Printing\Api\Cups\Exceptions')
+            ->not->toHaveSuffix('Exception')
+            ->toExtend(PrintingException::class)
+            ->toOnlyBeUsedIn('Rawilk\Printing\Api\Cups');
+
+        arch('types')->expect('Rawilk\Printing\Api\Cups\Types')
+            ->classes()
+            ->toExtend(Type::class);
+
+        arch('resources')->expect('Rawilk\Printing\Api\Cups\Resources')
+            ->classes()
+            ->toExtend(CupsObject::class);
+
+        arch('services')->expect('Rawilk\Printing\Api\Cups\Service')
+            ->classes()
+            ->toExtend(CupsAbstractService::class)->ignoring([CupsServiceFactory::class])
+            ->toHaveSuffix('Service')->ignoring([CupsServiceFactory::class]);
+    });
+
+    describe('printnode api', function () {
+        arch('enums')->expect('Rawilk\Printing\Api\PrintNode\Enums')
+            ->toBeEnums()
+            ->not->toHaveSuffix('Enum');
+
+        arch('exceptions')->expect('Rawilk\Printing\Api\PrintNode\Exceptions')
+            ->toImplement(ExceptionInterface::class)
+            ->not->toHaveSuffix('Exception');
+
+        arch('resources')->expect('Rawilk\Printing\Api\PrintNode\Resources')
+            ->classes()
+            ->toExtend(PrintNodeObject::class);
+
+        arch('resource concerns')->expect('Rawilk\Printing\Api\PrintNode\Resources\Concerns')
+            ->toBeTraits()
+            ->toOnlyBeUsedIn('Rawilk\Printing\Api\PrintNode\Resources');
+
+        arch('resource operations')->expect('Rawilk\Printing\Api\PrintNode\Resources\ApiOperations')
+            ->toBeTraits()
+            ->toOnlyBeUsedIn([
+                'Rawilk\Printing\Api\PrintNode\Resources',
+                PrintNodeApiResource::class,
+            ]);
+
+        arch('services')->expect('Rawilk\Printing\Api\PrintNode\Service')
+            ->classes()
+            ->toExtend(PrintNodeAbstractService::class)->ignoring([PrintNodeServiceFactory::class])
+            ->toHaveSuffix('Service')->ignoring([PrintNodeServiceFactory::class]);
+    });
+})->skip(! function_exists('arch'), 'arch() function not available with current pest version');
