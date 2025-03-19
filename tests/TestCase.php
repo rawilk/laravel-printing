46fb16e5fe0341920fb0e0d650613a1806c3d026
Randall Wilk <22842525+rawilk@users.noreply.github.com>
2025-03-19 09:01:27 -0500

    Ensure PrintNode API key is configured to at least something in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,6 +15,8 @@ class TestCase extends Orchestra
         $this->loadEnvironmentVariables();
 
         parent::setUp();
+
+        $this->ensureDriversAreConfigured();
     }
 
     protected function getPackageProviders($app): array
@@ -33,5 +35,18 @@ class TestCase extends Orchestra
         $dotEnv = Dotenv::createImmutable(__DIR__ . '/..');
 
         $dotEnv->load();
+    }
+
+    /**
+     * Set fake credentials for drivers if the config values are not set. Useful
+     * for PRs from forks that don't have access to repository secrets. This will
+     * help prevent value checking from throwing exceptions for missing api keys
+     * when they are only needed to create the client instance in the test.
+     */
+    protected function ensureDriversAreConfigured(): void
+    {
+        if (blank(config('printing.drivers.printnode.key'))) {
+            config()->set('printing.drivers.printnode.key', 'fake');
+        }
     }
 }
