48b58addd281b127f679d0a6a39cac79549288bc
Randall Wilk <22842525+rawilk@users.noreply.github.com>
2025-06-09 08:25:29 -0500

    Cast CUPS port in config to an int

--- config/printing.php
+++ config/printing.php
@@ -34,7 +34,7 @@ return [
             'ip' => env('CUPS_SERVER_IP'),
             'username' => env('CUPS_SERVER_USERNAME'),
             'password' => env('CUPS_SERVER_PASSWORD'),
-            'port' => env('CUPS_SERVER_PORT'),
+            'port' => (int) env('CUPS_SERVER_PORT'),
             'secure' => env('CUPS_SERVER_SECURE'),
         ],
 
